256a81e621fc35346011fa42fb72c755aff9427b
seanblue <5884067+seanblue@users.noreply.github.com>
2017-07-21 21:16:10 -0400

    Tweak description

--- lesson-filter.js
+++ lesson-filter.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          WaniKani Lesson Filter
 // @namespace     https://www.wanikani.com
-// @description   Specify how many lessons of each type you want to take.
+// @description   Specify how many lessons of each type you want to do.
 // @version       1.0.0
 // @include       *://www.wanikani.com/lesson/session*
 // @grant         none
