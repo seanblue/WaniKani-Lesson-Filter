1100be1bc9863688cc502962e2ea8502587c8ea3
= <5884067+seanblue@users.noreply.github.com>
2022-03-26 14:16:12 -0400

    Use 'let' and 'const' instead of 'var'

--- lesson-filter.js
+++ lesson-filter.js
@@ -17,26 +17,26 @@ const eventPrefix = 'seanblue.lessonfilter.';
 (function($) {
 	'use strict';
 
-	var localStorageSettingsKey = 'lessonFilter_inputData';
-	var localStorageSettingsVersion = 2;
+	const localStorageSettingsKey = 'lessonFilter_inputData';
+	const localStorageSettingsVersion = 2;
 
-	var propModifiedEvent = 'lessonFilter.propModified';
-	var queueUpdatedEvent = 'lessonFilter.queueUpdated';
+	const propModifiedEvent = 'lessonFilter.propModified';
+	const queueUpdatedEvent = 'lessonFilter.queueUpdated';
 
-	var activeQueueKey = 'l/activeQueue';
-	var inactiveQueueKey = 'l/lessonQueue';
-	var batchSizeKey = 'l/batchSize';
-	var unreadIndicesKey = 'l/unreadIndices';
-	var radicalCountKey = 'l/count/rad';
-	var kanjiCountKey = 'l/count/kan';
-	var vocabCountKey = 'l/count/voc';
+	const activeQueueKey = 'l/activeQueue';
+	const inactiveQueueKey = 'l/lessonQueue';
+	const batchSizeKey = 'l/batchSize';
+	const unreadIndicesKey = 'l/unreadIndices';
+	const radicalCountKey = 'l/count/rad';
+	const kanjiCountKey = 'l/count/kan';
+	const vocabCountKey = 'l/count/voc';
 
-	var batchSizeInputSelector = '#lf-batch-size';
-	var radicalInputSelector = '#lf-radicals';
-	var kanjiInputSelector = '#lf-kanji';
-	var vocabInputSelector = '#lf-vocab';
+	const batchSizeInputSelector = '#lf-batch-size';
+	const radicalInputSelector = '#lf-radicals';
+	const kanjiInputSelector = '#lf-kanji';
+	const vocabInputSelector = '#lf-vocab';
 
-	var style =
+	const style =
 		'<style>' +
 			'#lf-main { padding: 10px 0px; border-radius: 6px; margin: 5px; text-align: center; font-size: 0.8em; background-color: #444; color: #fff; }' +
 			'#lf-main input:focus { outline: none; }' +
@@ -51,7 +51,7 @@ const eventPrefix = 'seanblue.lessonfilter.';
 			'.lf-batch-size { background-color: #ff5500; }' +
 		'</style>';
 
-	var html =
+	const html =
 		'<div id="lf-main"">' +
 			'<div class="lf-title">Items to Learn</div>' +
 			'<div class="lf-list">' +
@@ -87,20 +87,20 @@ const eventPrefix = 'seanblue.lessonfilter.';
 	}
 
 	function loadSavedInputData() {
-		var savedDataString = localStorage[localStorageSettingsKey];
+		let savedDataString = localStorage[localStorageSettingsKey];
 
 		if (!savedDataString) {
 			return;
 		}
 
-		var savedData = JSON.parse(savedDataString);
+		let savedData = JSON.parse(savedDataString);
 
 		if (savedData.version !== localStorageSettingsVersion) {
 			delete localStorage[localStorageSettingsKey];
 			return;
 		}
 
-		var data = savedData.data;
+		let data = savedData.data;
 		$(batchSizeInputSelector).val(data.batchSize);
 		$(radicalInputSelector).val(data.radicals);
 		$(kanjiInputSelector).val(data.kanji);
@@ -114,7 +114,7 @@ const eventPrefix = 'seanblue.lessonfilter.';
 	}
 
 	function applyFilter(e) {
-		var rawFilterValues = getRawFilterValuesFromUI();
+		let rawFilterValues = getRawFilterValuesFromUI();
 		filterLessonsInternal(rawFilterValues);
 		saveRawFilterValues(rawFilterValues);
 
@@ -122,7 +122,7 @@ const eventPrefix = 'seanblue.lessonfilter.';
 	}
 
 	function filterLessonsInternal(rawFilterValues) {
-		var filterCounts = getFilterCounts(rawFilterValues);
+		let filterCounts = getFilterCounts(rawFilterValues);
 
 		if (filterCounts.nolessons) {
 			alert('You cannot remove all lessons');
@@ -131,7 +131,7 @@ const eventPrefix = 'seanblue.lessonfilter.';
 
 		updateBatchSize(filterCounts.batchSize);
 
-		var queue = getQueue();
+		let queue = getQueue();
 		filterQueue(queue, filterCounts);
 
 		updateQueue(queue);
@@ -148,10 +148,10 @@ const eventPrefix = 'seanblue.lessonfilter.';
 	}
 
 	function getFilterCounts(rawFilterValues) {
-		var radicalCount = getFilterCount(radicalCountKey, rawFilterValues.radicals);
-		var kanjiCount = getFilterCount(kanjiCountKey, rawFilterValues.kanji);
-		var vocabCount = getFilterCount(vocabCountKey, rawFilterValues.vocab);
-		var checkedBatchSize = getCheckedBatchSize(rawFilterValues.batchSize);
+		let radicalCount = getFilterCount(radicalCountKey, rawFilterValues.radicals);
+		let kanjiCount = getFilterCount(kanjiCountKey, rawFilterValues.kanji);
+		let vocabCount = getFilterCount(vocabCountKey, rawFilterValues.vocab);
+		let checkedBatchSize = getCheckedBatchSize(rawFilterValues.batchSize);
 
 		return {
 			'radicals': radicalCount,
@@ -163,8 +163,8 @@ const eventPrefix = 'seanblue.lessonfilter.';
 	}
 
 	function getFilterCount(key, rawValue) {
-		var currentCount = getWaniKaniData(key);
-		var value = parseInt(rawValue);
+		let currentCount = getWaniKaniData(key);
+		let value = parseInt(rawValue);
 
 		if (isNaN(value) || value > currentCount) {
 			return currentCount;
@@ -178,8 +178,8 @@ const eventPrefix = 'seanblue.lessonfilter.';
 	}
 
 	function getCheckedBatchSize(rawValue) {
-		var currentCount = getWaniKaniData(batchSizeKey);
-		var value = parseInt(rawValue);
+		let currentCount = getWaniKaniData(batchSizeKey);
+		let value = parseInt(rawValue);
 
 		if (isNaN(value)) {
 			return currentCount;
@@ -203,8 +203,8 @@ const eventPrefix = 'seanblue.lessonfilter.';
 	}
 
 	function filterQueueForType(queue, typePropertyName, itemsToKeep) {
-		var i;
-		var itemsKept = 0;
+		let i;
+		let itemsKept = 0;
 		for (i = 0; i < queue.length; i++) {
 			if (queue[i][typePropertyName] === undefined) {
 				continue;
@@ -227,7 +227,7 @@ const eventPrefix = 'seanblue.lessonfilter.';
 	}
 
 	function shuffleLessonsInternal() {
-		var queue = getQueue();
+		let queue = getQueue();
 		shuffle(queue);
 		updateQueue(queue);
 	}
@@ -235,9 +235,9 @@ const eventPrefix = 'seanblue.lessonfilter.';
 	function shuffle(array) {
 		// https://stackoverflow.com/a/12646864
 		// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
-		for (var i = array.length - 1; i > 0; i--) {
-			var j = Math.floor(Math.random() * (i + 1));
-			var temp = array[i];
+		for (let i = array.length - 1; i > 0; i--) {
+			let j = Math.floor(Math.random() * (i + 1));
+			let temp = array[i];
 			array[i] = array[j];
 			array[j] = temp;
 		}
@@ -248,9 +248,9 @@ const eventPrefix = 'seanblue.lessonfilter.';
 	}
 
 	function updateQueue(queue) {
-		var batchSize = getWaniKaniData(batchSizeKey);
-		var activeQueue = queue.slice(0, batchSize);
-		var inactiveQueue = queue.slice(batchSize);
+		let batchSize = getWaniKaniData(batchSizeKey);
+		let activeQueue = queue.slice(0, batchSize);
+		let inactiveQueue = queue.slice(batchSize);
 
 		// Must update the inactive queue after the active queue to get the UI to update properly.
 		setWaniKaniData(activeQueueKey, activeQueue);
@@ -282,7 +282,7 @@ const eventPrefix = 'seanblue.lessonfilter.';
 	}
 
 	function saveRawFilterValues(rawFilterValues) {
-		var settings = {
+		let settings = {
 			'version': localStorageSettingsVersion,
 			'data': rawFilterValues
 		};
@@ -304,10 +304,10 @@ const eventPrefix = 'seanblue.lessonfilter.';
 
 	// https://stackoverflow.com/a/14084869
 	function setEventToTrigger(jQueryMethodName, eventName) {
-		var originalMethod = $.fn[jQueryMethodName];
+		let originalMethod = $.fn[jQueryMethodName];
 
 		$.fn[jQueryMethodName] = function() {
-			var result = originalMethod.apply(this, arguments);
+			let result = originalMethod.apply(this, arguments);
 			$(this).trigger(eventName);
 
 			return result;
@@ -323,7 +323,7 @@ const eventPrefix = 'seanblue.lessonfilter.';
 	};
 
 	window.filterLessons = function(batchSize, radicalCount, kanjiCount, vocabCount) {
-		var rawFilterValues = {
+		let rawFilterValues = {
 			'batchSize': batchSize,
 			'radicals': radicalCount,
 			'kanji': kanjiCount,
